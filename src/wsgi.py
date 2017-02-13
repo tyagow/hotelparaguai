4bd845dba8a163701b1d6369ae4338795e6497ae
Tiago Almeida <tyagow@hotmail.com.br>
2017-02-13 13:41:24 -0200

    wsgi updated to work with in dev e prod

--- src/wsgi.py
+++ src/wsgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
+from dj_static import Cling, MediaCling
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
-application = Cling(get_wsgi_application())
-
+if settings.DEBUG:
+    application = Cling(MediaCling(get_wsgi_application()))
+else:
+    application = get_wsgi_application()
